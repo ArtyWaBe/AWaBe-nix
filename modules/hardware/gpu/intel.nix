a342e6c57e68a86f58244d9dae3d618f19ff138f
e <hororr646@gmail.com>
2025-06-14 02:53:48 +0300

    semicolon

--- modules/hardware/gpu/intel.nix
+++ modules/hardware/gpu/intel.nix
@@ -4,6 +4,6 @@
     hardware.intel-gpu-tools.enable = true;
     hardware.graphics.extraPackages = with pkgs; [
         intel-media-driver
-    ]
+    ];
     hardware.cpu.intel.updateMicrocode = lib.mkDefault config.boot.initrd.enable;
 }
