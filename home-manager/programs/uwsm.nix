31a7f2a22c0438328ab2ed3e20722d1e175feca9
e <hororr646@gmail.com>
2025-06-13 16:29:44 +0300

    restructuring, adding uwsm.nix for sending env variables to hyprland

---
+++ home-manager/programs/uwsm.nix
@@ -0,0 +1,21 @@
+{lib, config, ...}:
+
+let
+  inherit (lib) mkOption concatStringsSep;
+  inherit (lib.types) anything attrsOf;
+  inherit (lib.attrsets) mapAttrsToList;
+
+  cfg = config.home.uwsm;
+in {
+  options.home.uwsm = {
+    env = mkOption {
+      type = attrsOf anything;
+      description = "environment variables for uwsm";
+      default = {};
+    };
+  };
+
+  home.file.".config/uwsm/env".text = concatStringsSep "\n" (
+    mapAttrsToList (name: value: "export ${name}=${toString value}") cfg.env
+  ); 
+}
