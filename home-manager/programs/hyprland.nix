71f9e9414f9666ca64c4e7270292d8c3ca888720
e <hororr646@gmail.com>
2025-06-14 03:23:09 +0300

    remove monitor from hyprland.nix to modulirze it

--- home-manager/programs/hyprland.nix
+++ home-manager/programs/hyprland.nix
@@ -115,7 +115,7 @@
 
                     sensitivity = -0.7; # -1.0 - 1.0, 0 means no modification.
 
-                    touchpad {
+                    touchpad = {
                         natural_scroll = false;
                     };
                 };
