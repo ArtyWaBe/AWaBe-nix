0b5472703308daeabdce30f2399f982d6b272e6c
e <hororr646@gmail.com>
2025-06-14 03:47:00 +0300

    semicolon

--- home-manager/users/laptop-nachko.nix
+++ home-manager/users/laptop-nachko.nix
@@ -6,7 +6,7 @@
         ../programs/uwsm.nix
     ];
 
-    wayland.windowManager.hyprland = {
+    wayland.windowManager.hyprland.settings = {
         #Setting host specific monitor settings
         monitor = [ 
             "eDP-2, 2560x1440@165.00Hz 0x0, 1"
